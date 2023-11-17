0d4de0a6e324bae4b1d2ed5412157361086c76e1
Mihkel Rebane <mihkel.fox@gmail.com>
2023-11-17 18:05:29 +0200

    WIP spam

--- src/App.js
+++ src/App.js
@@ -9,11 +9,14 @@ function App() {
           <img src='/Images/background-3.png' alt='Secondary Background' className="background-2"/>
         </div>
         <div className="page-container">
-            <div className='Name'>Mihkel Rebane</div>
-            <div className='jobTitle'>Junior Web Developer</div>
-            <div className='Portfolio'>Portfolio</div>
+            <div className='Name'>Mihkel Rebane - WIP</div>
+            <div>WIP</div>
+            <div>WIP</div>
+            <div>WIP</div>
+            <div className='jobTitle'>Junior Web Developer WIP</div>
+            <div className='Portfolio'>Portfolio work in progress</div>
             <Menu/>
-            <div className='footer'>My Footer</div>
+            <div className='footer'>WIP</div>
         </div>
       </div>
   );
@@ -24,16 +27,16 @@ const Menu = class extends React.Component {
     return (
       <div className="menu-container">
         <div className="square square-main square1">Home</div>
-        <div className="square square-secondary-right square2"></div>
-        <div className="square square-secondary-left square3"></div>
-        <div className="square square-secondary-middle square4"></div>
+        <div className="square square-secondary-right square2">WIP</div>
+        <div className="square square-secondary-left square3">WIP</div>
+        <div className="square square-secondary-middle square4">WIP</div>
         <div className="square square-main square5">About</div>
         <div className="square square-main square6">Projects</div>
-        <div className="square square-secondary-right square7"></div>
-        <div className="square square-secondary-left square8"></div>
-        <div className="square square-secondary-middle square9"></div>
+        <div className="square square-secondary-right square7">WIP</div>
+        <div className="square square-secondary-left square8">WIP</div>
+        <div className="square square-secondary-middle square9">WIP</div>
         <div className="square square-main square10">CV</div>
-        <div className="square square-secondary-right square11"></div>
+        <div className="square square-secondary-right square11">WIP</div>
       </div>
     );
   }
