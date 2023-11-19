4c3eab8b38396f4ed3daeace073c0b9acf103c86
Mihkel Rebane <mihkel.fox@gmail.com>
2023-11-19 22:04:31 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -34,8 +34,8 @@ class App extends React.Component {
       <div>
         <div>
           <img src='/Images/background-3.webp' alt='Secondary Background' className='background-3' />
-          <img src='/Images/outline-top.png' alt='White Outline Top' className='outline-top' />
-          <img src='/Images/outline-bottom.png' alt='White Outline Bottom' className='outline-bottom' />
+          <img src='/Images/outline-top.webp' alt='White Outline Top' className='outline-top' />
+          <img src='/Images/outline-bottom.webp' alt='White Outline Bottom' className='outline-bottom' />
         </div>
         <div className='header'></div>
         <div className="page-container">
