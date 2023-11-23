e06573987d3f03b07a49767706e08d7421927a7e
Mihkel Rebane <mihkel.fox@gmail.com>
2023-11-23 05:39:48 +0200

    Tweaks and preliminary Responsiveness

--- src/App.js
+++ src/App.js
@@ -51,37 +51,35 @@ class App extends React.Component {
   render() {
     const { activeMenu } = this.state;
     return (
-      
-      <div>
-        <div>
-        <div className='WIP'>Currently a WIP and not yet mobile friendly, will work on that next</div>
-          <img src='/Images/background-3.webp' alt=' Background' className='background-3' />
-          <img src='/Images/outline-top.webp' alt='White Outline Top' className='outline-top' />
-          <img src='/Images/outline-bottom.webp' alt='White Outline Bottom' className='outline-bottom' />
-        </div>
-        <div className='header'></div>
-        <div className="page-container">
-          <div className='Name'><img src='/Images/Header/nimi.svg' alt='Mihkel Rebane' /></div>
-          <div className='jobTitle'><img src='/Images/Header/amet.svg' alt='Junior Web-Developer' /></div>
-          <div className='Portfolio'><img src='/Images/Header/portfolio.svg' alt='Portfolio' /></div>
-          <div id="main"></div>
-          <div className='footer'>© 2023 MihkelRebane.com - development began 16.11.2023</div>
-
-          {activeMenu === 'Menu' && <Menu
-                                              onMenuChange={this.handleMenuChange}
-                                              handleButtonClick={this.handleButtonClick} />}
-          {activeMenu === 'MenuAbout' && <MenuAbout
-                                              onMenuChange={this.handleMenuChange}
-                                              handleButtonClick={this.handleButtonClick}
-                                              handleEmailClick={this.handleEmailClick} />}
-          {activeMenu === 'MenuProject' && <MenuProject
-                                              onMenuChange={this.handleMenuChange}
-                                              handleButtonClick={this.handleButtonClick}
-                                              handleProjectClick1={this.handleProjectClick1}
-                                              handleProjectClick2={this.handleProjectClick2}
-                                              handleProjectClick3={this.handleProjectClick3}
-                                              handleProjectClick4={this.handleProjectClick4}/>}
-        </div>
+      <div className='ThePage'>
+        {/* <div className='WIP'>Currently a WIP and not yet mobile friendly, will work on that next</div> */}
+            <div className='ImagesDiv'>
+              <img src='/Images/outline-top.webp' alt='White Outline Top' className='outline-top' />
+              <img src='/Images/outline-bottom.webp' alt='White Outline Bottom' className='outline-bottom' />
+            </div>
+            <div className="page-container">
+              <div className='header'>
+                <svg className='Name'><use xlinkHref='/Images/Header/headertext.svg#NameSVG' /></svg>
+                <svg className='jobTitle'><use xlinkHref='/Images/Header/headertext.svg#TitleSVG' /></svg>
+                <svg className='Portfolio'><use xlinkHref='/Images/Header/headertext.svg#PortfolioSVG' /></svg>
+              </div>
+              {activeMenu === 'Menu' && <Menu
+                                                  onMenuChange={this.handleMenuChange}
+                                                  handleButtonClick={this.handleButtonClick} />}
+              {activeMenu === 'MenuAbout' && <MenuAbout
+                                                  onMenuChange={this.handleMenuChange}
+                                                  handleButtonClick={this.handleButtonClick}
+                                                  handleEmailClick={this.handleEmailClick} />}
+              {activeMenu === 'MenuProject' && <MenuProject
+                                                  onMenuChange={this.handleMenuChange}
+                                                  handleButtonClick={this.handleButtonClick}
+                                                  handleProjectClick1={this.handleProjectClick1}
+                                                  handleProjectClick2={this.handleProjectClick2}
+                                                  handleProjectClick3={this.handleProjectClick3}
+                                                  handleProjectClick4={this.handleProjectClick4}/>}
+              <div className='footer'>© 2023 MihkelRebane.com - development began 16.11.2023</div>
+            </div>
+            <div  className='background-3' ><img src='/Images/background-3.webp' alt=' Background'/></div>
       </div>
     );
   }
@@ -91,32 +89,34 @@ class Menu extends Component {
 
 	render() {
 		return (
-		  <div className="menu-container">
-        <div className="square sq-main sq1 hover" onClick={() => this.props.onMenuChange('Menu')}>
-          <p className='button-text'>
-          <img src='/Images/Buttons/home.svg' alt='Home'/></p>
+      <div className='menu-container-wrapper'>
+        <div className="menu-container">
+          <div className="sq-main sq1 hover" onClick={() => this.props.onMenuChange('Menu')}></div>
+            <div className='button-text sq1'>
+                <img src='/Images/Buttons/home.svg' alt='Home'/>
+            </div>
+          <div className="sq-main sq5 hover" onClick={() => this.props.onMenuChange('MenuAbout')}></div>
+            <div className='button-text sq5'>
+                <img src='/Images/Buttons/about.svg' alt='About'/>
+            </div>
+          <div className="sq-main sq6 hover" onClick={() => this.props.onMenuChange('MenuProject')}></div>
+            <div className='button-text sq6'>
+                <img src='/Images/Buttons/projects.svg' alt='Projects'/>
+            </div>
+          <div className="sq-main sq10 hover" onClick={this.props.handleButtonClick}></div>
+          <div className='button-text sq10'>
+            <img src='/Images/Buttons/cv.svg' alt='CV' className="CV"/>
+            <img src='/Images/Buttons/Download.svg' alt='Resume' className="DL"/>
+          </div>
+          <div className="sq-left sq3"></div>
+          <div className="sq-left sq8"></div>
+          <div className="sq-middle sq4"></div>
+          <div className="sq-middle sq9"></div>
+          <div className="sq-right sq2"></div>
+          <div className="sq-right sq7"></div>
+          <div className="sq-right sq11"></div>
         </div>
-        <div className="square sq-main sq5 hover" onClick={() => this.props.onMenuChange('MenuAbout')}>
-          <p className='button-text'>
-          <img src='/Images/Buttons/about.svg' alt='About'/></p>
-        </div>
-        <div className="square sq-main sq6 hover" onClick={() => this.props.onMenuChange('MenuProject')}>
-          <p className='button-text'>
-          <img src='/Images/Buttons/projects.svg' alt='Projects'/></p>
-        </div>
-        <div className="square sq-main sq10 hover" onClick={this.props.handleButtonClick}>
-          <p className='button-text-cv'>
-          <img src='/Images/Buttons/cv.svg' alt='CV'/></p>
-          <img src='/Images/Buttons/Download.svg' alt='Resume' className="CV"/>
-        </div>
-        <div className="square sq-left sq3"></div>
-        <div className="square sq-left sq8"></div>
-        <div className="square sq-middle sq4"></div>
-        <div className="square sq-middle sq9"></div>
-        <div className="square sq-right sq2"></div>
-        <div className="square sq-right sq7"></div>
-        <div className="square sq-right sq11"></div>
-		  </div>
+      </div>
 		);
   }
 }
@@ -158,54 +158,53 @@ class MenuAbout extends Component {
     this.setState({ isModalOpen: false });
   };
 
-	render() {
+  render() {
 		return (
-		  <div className="menu-container">
-        <div className="square sq-main sq1 hover" onClick={() => this.props.onMenuChange('Menu')}>
-          <p className='button-text'>
-          <img src='/Images/Buttons/home.svg' alt='Home'/></p>
+      <div className='menu-container-wrapper'>
+        <div className="menu-container">
+          <div className="sq-main sq1 hover" onClick={() => this.props.onMenuChange('Menu')}></div>
+            <div className='button-text sq1'>
+                <img src='/Images/Buttons/home.svg' alt='Home'/>
+            </div>
+            <div className="sq-main sq5 hover" onClick={() => this.props.onMenuChange('Menu')}></div>
+              <div className='button-text sq5'>
+                  <img src='/Images/Buttons/about.svg' alt='About'/>
+              </div>
+            <div className="sq-main sq6 hover" onClick={() => this.props.onMenuChange('MenuProject')}></div>
+              <div className='button-text sq6'>
+                  <img src='/Images/Buttons/projects.svg' alt='Projects'/>
+              </div>
+            <div className="sq-main sq10 hover" onClick={this.props.handleButtonClick}></div>
+            <div className='button-text sq10'>
+                <img src='/Images/Buttons/cv.svg' alt='CV' className="CV"/>
+              <img src='/Images/Buttons/Download.svg' alt='Resume' className="DL"/>
+            </div>
+            <div
+                className={`sq-left sq3 hover`}
+                style={{ cursor: 'pointer' }}
+                onClick={this.handleOpenModal}>
+            </div>
+            <div className="button-text sq3">An Introduction</div>
+          <ReactModal
+            isOpen={this.state.isModalOpen}
+            onRequestClose={this.handleCloseModal}
+            contentLabel="Modal - Who is Mihkel"
+            className="Introduction"
+            overlayClassName="Overlay">
+            {this.state.textContent ? (
+              <div className='IntroductionModal' dangerouslySetInnerHTML={{ __html: this.state.textContent }} />
+            ) : (
+              <p>Loading content...</p>
+            )}
+          </ReactModal>
+          <div className="sq-left sq8 hover" style={{ cursor: 'pointer' }} onClick={this.props.handleEmailClick}></div>
+          <div className="button-text sq8">Email Me</div>
+          <div className="sq-middle sq4"></div>
+          <div className="sq-middle sq9"></div>
+          <div className="sq-right sq2"></div>
+          <div className="sq-right sq7"></div>
+          <div className="sq-right sq11"></div>
         </div>
-        <div className="square sq-main sq5 hover" onClick={() => this.props.onMenuChange('Menu')}>
-          <p className='button-text'>
-          <img src='/Images/Buttons/about.svg' alt='About'/></p>
-        </div>
-        <div className="square sq-main sq6 hover" onClick={() => this.props.onMenuChange('MenuProject')}>
-          <p className='button-text'>
-          <img src='/Images/Buttons/projects.svg' alt='Projects'/></p>
-        </div>
-        <div className="square sq-main sq10 hover" onClick={this.props.handleButtonClick}>
-          <p className='button-text-cv'>
-          <img src='/Images/Buttons/cv.svg' alt='CV'/></p>
-          <img src='/Images/Buttons/Download.svg' alt='Resume' className="CV"/>
-        </div>
-        <div
-          className={`square sq-left sq3  hover`}
-          style={{ cursor: 'pointer' }}
-          onClick={this.handleOpenModal}>
-          <p className="button-text">An Introduction</p>
-        </div>
-
-        <ReactModal
-          isOpen={this.state.isModalOpen}
-          onRequestClose={this.handleCloseModal}
-          contentLabel="Modal - Who is Mihkel"
-          className="Introduction"
-          overlayClassName="Overlay">
-          {this.state.textContent ? (
-            <div className='IntroductionModal' dangerouslySetInnerHTML={{ __html: this.state.textContent }} />
-          ) : (
-            <p>Loading content...</p>
-          )}
-        </ReactModal>
-
-        <div className="square sq-left sq8 hover" style={{ cursor: 'pointer' }} onClick={this.props.handleEmailClick}>
-          <p className="button-text">Email Me</p>
-        </div>
-        <div className="square sq-middle sq4"></div>
-        <div className="square sq-middle sq9"></div>
-        <div className="square sq-right sq2"></div>
-        <div className="square sq-right sq7"></div>
-        <div className="square sq-right sq11"></div>
 		  </div>
 		);
   }
@@ -213,63 +212,55 @@ class MenuAbout extends Component {
 
 class MenuProject extends Component {
   render() {
-    return (
-		  <div className="menu-container">
-
-        <div
-          className="square sq-main sq1 hover"
-          onClick={() => this.props.onMenuChange('Menu')}>
-          <p className='button-text'>
-          <img src='/Images/Buttons/home.svg' alt='Home'/></p>
+		return (
+      <div className='menu-container-wrapper'>
+        <div className="menu-container">
+          <div className="sq-main sq1 hover" onClick={() => this.props.onMenuChange('Menu')}></div>
+            <div className='button-text sq1'>
+                <img src='/Images/Buttons/home.svg' alt='Home'/>
+            </div>
+          <div className="sq-main sq5 hover" onClick={() => this.props.onMenuChange('MenuAbout')}></div>
+            <div className='button-text sq5'>
+                <img src='/Images/Buttons/about.svg' alt='About'/>
+            </div>
+          <div className="sq-main sq6 hover" onClick={() => this.props.onMenuChange('Menu')}></div>
+            <div className='button-text sq6'>
+                <img src='/Images/Buttons/projects.svg' alt='Projects'/>
+            </div>
+          <div className="sq-main sq10 hover" onClick={this.props.handleButtonClick}></div>
+          <div className='button-text sq10'>
+              <img src='/Images/Buttons/cv.svg' alt='CV' className="CV"/>
+            <img src='/Images/Buttons/Download.svg' alt='Resume' className="DL"/>
+          </div>
+          <div
+            className={`sq-left sq3 hover`}
+            style={{ cursor: 'pointer' }}
+            onClick={this.props.handleProjectClick1}>
+          </div>
+          <div className="button-text sq3">React - API</div>
+          <div
+            className="sq-left sq8 hover"
+            style={{ cursor: 'pointer' }}
+            onClick={this.props.handleProjectClick3}>
+          </div>
+          <div className='button-text sq8'>C++ Script</div>
+          <div
+            className="sq-middle sq4 hover"
+            style={{ cursor: 'pointer' }}
+            onClick={this.props.handleProjectClick2}>
+          </div>
+          <div className="button-text sq4">TicTacToe<br/>in React</div>
+          <div
+            className="sq-middle sq9 hover"
+            style={{ cursor: 'pointer' }}
+            onClick={this.props.handleProjectClick4}>
+          </div>
+          <div className="button-text sq9">Schoolwork</div>
+          <div className="sq-right sq2"></div>
+          <div className="sq-right sq7"></div>
+          <div className="sq-right sq11"></div>
         </div>
-        <div
-          className="square sq-main sq5 hover"
-          onClick={() => this.props.onMenuChange('MenuAbout')}>
-          <p className='button-text'>
-          <img src='/Images/Buttons/about.svg' alt='About'/></p>
-        </div>
-        <div
-          className="square sq-main sq6 hover"
-          onClick={() => this.props.onMenuChange('Menu')}>
-          <p className='button-text'>
-          <img src='/Images/Buttons/projects.svg' alt='Projects'/></p>
-        </div>
-        <div
-          className="square sq-main sq10 hover"
-          onClick={this.props.handleButtonClick}>
-          <p className='button-text-cv'>
-          <img src='/Images/Buttons/cv.svg' alt='CV'/></p>
-          <img src='/Images/Buttons/Download.svg' alt='Resume' className="CV"/>
-        </div>
-        <div
-          className={`square sq-left sq3 hover`}
-          style={{ cursor: 'pointer' }}
-          onClick={this.props.handleProjectClick1}>
-          <p className="button-text">React - API</p>
-        </div>
-        <div
-          className="square sq-left sq8 hover"
-          style={{ cursor: 'pointer' }}
-          onClick={this.props.handleProjectClick3}>
-          <p className="button-text">C++ Script</p>
-        </div>
-        <div
-          className="square sq-middle sq4 hover"
-          style={{ cursor: 'pointer' }}
-           onClick={this.props.handleProjectClick2}>
-          <p className="button-text">TicTacToe<br/>in React</p>
-        </div>
-
-        <div
-          className="square sq-middle sq9 hover"
-          style={{ cursor: 'pointer' }}
-          onClick={this.props.handleProjectClick4}>
-          <p className="button-text">Schoolwork</p>
-        </div>
-        <div className="square sq-right sq2"></div>
-        <div className="square sq-right sq7"></div>
-        <div className="square sq-right sq11"></div>
-		  </div>
+      </div>
 		);
   }
 }
